911a37df6d7db6ef0d5723fbb3808152e7b8e880
Benjamin J Anderson <banderson@mashable.com>
2019-07-08 20:39:52 -0500

    Add parser lookahead capabilities

--- inc/modules/parser/parser_module.h
+++ inc/modules/parser/parser_module.h
@@ -8,11 +8,14 @@
 
 typedef struct GambitParser {
   TokenArray* token_array;
+  Token* current_token;
   ASTNodePtr* root_node;
   BOOLEAN errored;
 } Parser;
 
 Parser* init_parser(TokenArray* tokens);
+Token* next_token(Parser* parser);
+Token* peek_token(Parser* parser, int n);
 void exit_parser(Parser* parser);
 
 #endif
--- src/modules/parser/parser_module.c
+++ src/modules/parser/parser_module.c
@@ -6,7 +6,21 @@ Parser* init_parser(TokenArray* tokens) {
   parser->errored = FALSE;
   parser->root_node = NULL;
 
+  parser->current_token = parser->token_array->array;
+
   return parser;
+}
+
+Token* next_token(Parser* parser) {
+  return ++(parser->current_token);  
+}
+
+Token* peek_token(Parser* parser, int n) {
+  Token* look_ahead = parser->current_token;
+  for (int i = 0; i < n; i++) {
+    look_ahead++;
+  }
+  return look_ahead;
 }
 
 void exit_parser(Parser* parser) {
